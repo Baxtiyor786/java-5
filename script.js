5862c621f4f1b4aad95073c8e51187d798eaedd5
Baxtiyor786 <jomoliddinovbaxtiyor@gmail.com>
2024-12-24 18:15:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let oot = prompt("ismingizni kiriting")
+
+let spac = prompt("space di idni kiriting")
+
+let passwo = prompt("space dagi passwordingiz")
+
+let all = {
+    name: oot,
+    id: spac,
+    password: passwo
+}
+
+console.log(all);
